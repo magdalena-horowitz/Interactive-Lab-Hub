ee4f814bc3d2dd902fc6bdc61eeb98741d6f92c7
magdalena-horowitz <maggieyhorowitz@gmail.com>
2021-09-30 21:22:42 -0400

    Add Scripts for Part 1: TTS and STT in added_scripts folder

--- Lab 3/added_scripts/STT_phone_number.sh
+++ Lab 3/added_scripts/STT_phone_number.sh
@@ -1,3 +1,12 @@
 # from https://elinux.org/RPi_Text_to_Speech_(Speech_Synthesis)
-espeak -ven+f2 -k5 -s150 --stdout  "Hi Magdalena" | aplay
+
+
+
+#Added Script 2 For Part 1: Magdalena Horowitz Lab 3
+# SST - input your phone number
+#uses the new file: input_STT.py to parse your phone number and save it and print it
+
+espeak -ven+f2 -k5 -s150 --stdout  "Tell me your phone number" | aplay
  
+arecord -D hw:2,0 -f cd -c1 -r 48000 -d 5 -t wav recorded_mono.wav
+python3 input_STT.py recorded_mono.wav
